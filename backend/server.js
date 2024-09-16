daa7b04765a6ed232436b4d749fc7ff78aeea6e2
Imam Alfarizi <imam.alfarizi.777@gmail.com>
2024-09-16 22:41:54 +0700

    #6 part 2

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,24 @@
-const express = require("express");
-const { chats } = require("./data/data");
-const dotenv = require("dotenv");
-const cors = require("cors");
+const express = require('express');
+const { chats } = require('./data/data');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const Chat = require('./models/chat.model');
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: ["http://localhost:5173"] }));
+app.use(cors({ origin: ['http://localhost:5173'] }));
 
-app.get("/", (req, res) => {
-  res.json("API is running");
+app.get('/', (req, res) => {
+  res.json('API is running');
 });
 
-app.get("/api/chat", (req, res) => {
+app.get('/api/chat', (req, res) => {
   res.json(chats);
 });
 
-app.get("/api/chat/:id", (req, res) => {
+app.get('/api/chat/:id', (req, res) => {
   const { id } = req.params;
   const singleChat = chats.find((chat) => chat._id === id);
   res.json(singleChat);
